e2fd95fdce00c10cca576b9f4f0fd1bfcd341c20
Jake Herman <thisguyherm@gmail.com>
2016-11-07 17:14:54 -0600

    completed robot map

--- src/org/usfirst/frc/team1710/robot/RobotMap.java
+++ src/org/usfirst/frc/team1710/robot/RobotMap.java
@@ -1,6 +1,11 @@
 package org.usfirst.frc.team1710.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
+
 public class RobotMap {
-	
-	
+	public Talon shooterR, shooterL, sucker, intakeL, intakeR;
+	public Joystick mechStick, driveStick;
+	public RobotDrive driveTrain;
 }
