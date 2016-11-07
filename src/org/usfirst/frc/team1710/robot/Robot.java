ccd18fdbaa8e5e7e963a0115831bcf2f009c334b
Jake Herman <thisguyherm@gmail.com>
2016-11-07 17:18:58 -0600

    completed robotmap

--- src/org/usfirst/frc/team1710/robot/Robot.java
+++ src/org/usfirst/frc/team1710/robot/Robot.java
@@ -2,6 +2,9 @@
 package org.usfirst.frc.team1710.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -13,6 +16,14 @@ public class Robot extends IterativeRobot {
     SendableChooser chooser;
 	
     public void robotInit() {
+    	RobotMap.driveStick = new Joystick(0);
+    	RobotMap.mechStick = new Joystick(1);
+    	RobotMap.driveTrain = new RobotDrive(2,6,4,8);
+    	RobotMap.shooterR = new Talon(1);
+    	RobotMap.shooterL = new Talon(9);
+    	RobotMap.sucker = new Talon(3);
+    	RobotMap.intakeL = new Talon(7);
+    	RobotMap.intakeR = new Talon(5);
     }
     
     public void autonomousInit() {
--- src/org/usfirst/frc/team1710/robot/RobotMap.java
+++ src/org/usfirst/frc/team1710/robot/RobotMap.java
@@ -5,7 +5,8 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 
 public class RobotMap {
-	public Talon shooterR, shooterL, sucker, intakeL, intakeR;
-	public Joystick mechStick, driveStick;
-	public RobotDrive driveTrain;
+	public static Talon shooterR, shooterL, sucker, intakeL, intakeR;
+	public static Joystick mechStick;
+	public static Joystick driveStick;
+	public static RobotDrive driveTrain;
 }
