6acdc991a4b408575cced3104d24805f0545ae30
spencersliffe <spencer.s1@ravenengineer.org>
2016-11-10 18:48:55 -0600

    First commit

---
+++ src/org/usfirst/frc/team1710/robot/Intake.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team1710.robot;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Intake extends Subsystem {
+    
+
+    public void initDefaultCommand() {
+    	RobotMap.intakeL.set(0);
+    	RobotMap.intakeR.set(0);
+    	RobotMap.sucker.set(0);
+    }
+    public void intakeSuck(double speed){
+    	RobotMap.sucker.set(speed);
+    
+    }
+    public void intakeLift(double speed){
+    	RobotMap.intakeL.set(speed);
+    	RobotMap.intakeR.set(-speed);
+    }
+}
+
