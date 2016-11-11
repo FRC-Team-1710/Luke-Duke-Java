f7792a4b4f98fd42d6d7f87d6591acc2fe709664
LukeCu <luke.c@ravenengineer.org>
2016-11-10 18:50:57 -0600

    yes

---
+++ src/org/usfirst/frc/team1710/robot/Shooter.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team1710.robot;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+public class Shooter extends Subsystem {
+
+    public void initDefaultCommand() {
+    	RobotMap.shooterR.set(0);
+    	RobotMap.shooterL.set(0);
+    	
+    }
+    public void shooterRun(double speed){
+    	RobotMap.shooterR.set(speed);
+    	RobotMap.shooterL.set(-speed);
+    
+}
+}
